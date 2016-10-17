fed634150d2b24fdfab10d25653e6ef52b45f9eb
Andre Costa <andre.andrade.costa@gmail.com>
2016-10-17 10:42:54 -0200

    Menu item added to AppBar.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import HelloContainer from './HelloContainer';
 
+
+
+
+function Menu(props) {
+    return (
+        <IconMenu
+            {...props}
+            iconButtonElement={
+                <IconButton><MoreVertIcon/></IconButton>
+            }
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+        >
+            <MenuItem primaryText="About" />
+            <MenuItem primaryText="Sign out" />
+        </IconMenu>
+    )
+}
+
+Menu.muiName = 'IconMenu';
 
 class Home extends Component {
     render() {
@@ -9,7 +33,7 @@ class Home extends Component {
             <div>
                 <AppBar
                     title="My Raspberry Pi Monitor"
-                    iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+                    iconElementRight={<Menu/>}/>
                 <HelloContainer/>
             </div>
         )
