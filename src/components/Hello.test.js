3104638b8d371386e801416b6bf98e032fd927b7
Andre Costa <andre.andrade.costa@gmail.com>
2016-10-19 11:35:10 -0200

    Removed unused code.

--- src/components/Hello.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Hello from './Hello';
-
-it('renders text', () => {
-    const wrapper = shallow(<Hello text="aaa"/>)
-    expect(wrapper.text()).toContain('Hello');
-})
