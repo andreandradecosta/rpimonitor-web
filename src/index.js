7bd823de5d72b6e5db825e9cb651ca93182ff07e
Andre Costa <andre.andrade.costa@gmail.com>
2016-10-20 13:16:44 -0200

    Routing.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import routes from './config/routes';
 import './index.css';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
 ReactDOM.render(
-    <App />,
+    routes,
     document.getElementById('root')
 );
