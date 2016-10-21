bc45b3968e162e323edd93cfcc0659fd38629aa4
Andre Costa <andre.andrade.costa@gmail.com>
2016-10-21 11:27:46 -0200

    ES6 syntax.

--- src/containers/InfoContainer.js
+++ src/containers/InfoContainer.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { getData } from '../helpers/api';
 
 export default function InfoContainer(resource, InfoComponent) {
-    return React.createClass({
-        getInitialState: function() {
-            return {
+
+    return class extends React.Component {
+
+        constructor(props) {
+            super(props);
+            this.state = {
                 isLoading: true,
                 data: {}
             }
-        },
+        }
 
-        componentDidMount: async function() {
+        async componentDidMount() {
             try {
                 const data = await getData(resource);
                 this.setState({
@@ -20,12 +23,12 @@ export default function InfoContainer(resource, InfoComponent) {
             } catch (err) {
                 console.warn('Error in InfoContainer', err);
             }
-        },
+        }
 
-        render: function() {
+        render() {
             return <InfoComponent
                         isLoading={this.state.isLoading}
                         data={this.state.data} />
         }
-    })
+    };
 }
