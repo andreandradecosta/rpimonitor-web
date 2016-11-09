8a2709fa2a8ede87d664c7dc5bf38b773fdd3182
Andre Costa <andre.andrade.costa@gmail.com>
2016-11-09 16:08:14 -0200

    Dispatch info action on fetch.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,28 @@
 import * as api from '../helpers/api';
 
 
+export const fetchData = (resource) => (dispatch) => {
+    dispatch({
+        type: 'FETCH_DATA_REQUEST',
+        resource
+    });
+    return api.getData(resource).then(
+        result => {
+            dispatch({
+                type: 'FETCH_DATA_SUCCESS',
+                resource,
+                result
+            });
+        },
+        error => {
+            dispatch({
+                type: 'FETCH_DATA_FAILURE',
+                message: `Request error: ${error.message}`
+            })
+        }
+    );
+}
+
 export const fetchHistory = (start, end) => (dispatch) => {
     dispatch({
         type: 'FETCH_HISTORY_REQUEST',
