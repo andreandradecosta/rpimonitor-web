75b33e57f8c1301c5952e6d1d39dc69f1a84d754
Andre Costa <andre.andrade.costa@gmail.com>
2016-11-10 15:59:57 -0200

    Logout action and send token on every request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,19 @@
 import * as api from '../helpers/api';
+import { getToken } from '../reducers/Auth';
 
+export const logout = () => {
+    localStorage.removeItem("token");
+    return {
+        type: 'LOGOUT_SUCCESS'
+    }
+}
 
-export const fetchInfo = (resource) => (dispatch) => {
+export const fetchInfo = (resource) => (dispatch, getState) => {
     dispatch({
         type: 'FETCH_INFO_REQUEST',
         resource
     });
-    return api.getInfo(resource).then(
+    return api.getInfo(resource, getToken(getState())).then(
         result => {
             dispatch({
                 type: 'FETCH_INFO_SUCCESS',
@@ -23,14 +30,14 @@ export const fetchInfo = (resource) => (dispatch) => {
     );
 }
 
-export const fetchHistory = (start, end) => (dispatch) => {
+export const fetchHistory = (start, end) => (dispatch, getState) => {
     dispatch({
         type: 'FETCH_HISTORY_REQUEST',
         start,
         end
 
     });
-    return api.getHistory(start, end).then(
+    return api.getHistory(start, end, getToken(getState())).then(
         result => {
             dispatch({
                 type: 'FETCH_HISTORY_SUCCESS',
