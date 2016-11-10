9fe46aa9154d3bdf28f65fc3baf11e11668660f3
Andre Costa <andre.andrade.costa@gmail.com>
2016-11-10 16:06:17 -0200

    Send token on every request.

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,16 +1,30 @@
 import axios from 'axios';
 import moment from 'moment';
 
-export const getInfo = (resource) =>
-     axios.get(`/api/${resource}`).then((response) => response.data);
+const headers = (token) => (
+    {
+        'authorization': `Bearer ${token}`
+    }
+)
+
+export const getInfo = (resource, token) =>
+     axios.get(
+         `/api/${resource}`,
+         {
+             headers: headers(token)
+         }
+     ).then((response) => response.data);
 
 const dateFormat = 'YYYY-MM-DD';
 
-export const getHistory = (start, end) =>
-    axios.get(`/api/history`, {
-        params: {
-            start: moment(start).format(dateFormat),
-            end: moment(end).format(dateFormat)
+export const getHistory = (start, end, token) =>
+    axios.get(
+        `/api/history`,
+        {
+            headers: headers(token),
+            params: {
+                start: moment(start).format(dateFormat),
+                end: moment(end).format(dateFormat)
+            }
         }
-    })
-    .then((response) => response.data);
+    ).then((response) => response.data);
