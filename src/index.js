0a75cd1e5e2e8cc8a7607f800159b15c766bb037
Andre Costa <andre.andrade.costa@gmail.com>
2016-11-11 15:04:00 -0200

    Refactory of configureStore to config folder.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import configureStore from './helpers/configureStore';
+import configureStore from './config/configureStore';
 
 const store = configureStore();
 
